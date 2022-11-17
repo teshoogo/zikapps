fdb03f45e9ca2ef51837c4b5fd28bc3e139b15cf
Tesh <mail.weslleythiago@gmail.com>
2022-11-17 00:56:20 -0300

    update

---
+++ aplicativos/gocontrol/application/menu-config.js
@@ -0,0 +1,41 @@
+// GoControl v2.0.0
+// Weslley Thiago (GoApps)
+// mail.weslleythiago@gmail.com
+
+const { ipcMain } = require('electron');
+
+module.exports = [
+    {
+        label: 'Ajuda',
+        submenu: [
+            {label : 'Início', click : () => { require('./main')("home") }},
+            {label : 'Sobre', click : () => { require('./main')("about") }},
+            {role : 'quit'},
+        ]
+    },
+    {
+        label: 'Editar',
+        submenu: [
+            {role : 'undo'},
+            {role : 'redo'},
+            {role : 'cut'},
+            {role : 'copy'},
+            {role : 'paste'},
+            { type: 'separator' },
+            {label : 'Print', click: printPage},
+        ]
+    },
+    {
+        label: 'Ver',
+        submenu: [
+            {role : 'reload'},
+            {role : 'zoomIn'},
+            {role : 'zoomOut'},
+        ]
+    },
+]
+
+// Print page method
+function printPage() {
+    ipcMain.emit('printPage')
+}
